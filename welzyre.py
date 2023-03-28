43c45752bdaa76974210494af4065eac4588ff95
epicjazsax <justin.zakrajsek@gmail.com>
2023-03-28 15:30:01 -0700

    added basic stat and advantage roll formula

---
+++ welzyre.py
@@ -0,0 +1,68 @@
+import random
+
+strength = 11
+dexterity = 16
+constitution = 15
+intelligence = 17
+wisdom = 9
+charisma = 12
+strmod = (strength//2) - 5
+dexmod = (dexterity//2) - 5
+conmod = (constitution//2) - 5
+intmod = (intelligence//2) - 5
+wismod = (wisdom//2) - 5
+chamod = (charisma//2) - 5
+#stat and modifier block
+
+def roll(die):
+    r1, r2 = 1, die
+    die_thing = list(range(r1, r2 + 1))
+    random.shuffle(die_thing)
+    roll_die = die_thing[0]
+    return roll_die
+def rollcheck():
+    statcheck = input("Rolling stat check - str, dex, con, int, wis, or cha: ")
+    die0 = roll(20)
+    print("roll is:", die0)
+    if statcheck == "str":
+        print("stat check is:", die0 + strmod)
+    elif statcheck == "dex":
+        print("stat check is:", die0 + dexmod)
+    elif statcheck == "con":
+        print("stat check is:", die0 + conmod)
+    elif statcheck == "int":
+        print("stat check is:", die0 + intmod)
+    elif statcheck == "wis":
+        print("stat check is:", die0 + wismod)
+    elif statcheck == "cha":
+        print("stat check is:", die0 + chamod)
+    else:
+        return "invalid input"
+    #with "if" statements, always returns 'invalid input'
+    #with "elif" statements, always returns 'none'
+    #how to make it only print if input is false?
+
+def rolladv():
+    advcheck = input("Rolling check with advantage - str, dex, con, int, wis, or cha: ")
+    die1, die2 = roll(20), roll(20)
+    if die1 > die2:
+        advdie = die1
+    else:
+        advdie = die2
+    print("rolls are:", die1, die2)
+    if advcheck == "str":
+        print("advantage check is:", advdie + strmod)
+    if advcheck == "dex":
+        print("advantage check is:", advdie + dexmod)
+    if advcheck == "con":
+        print("advantage check is:", advdie + conmod)
+    if advcheck == "int":
+        print("advantage check is:", advdie + intmod)
+    if advcheck == "wis":
+        print("advantage check is:", advdie + wismod)
+    if advcheck == "cha":
+        print("advantage check is:", advdie + chamod)
+    else:
+        return "invalid input"
+
+print(rollcheck())
