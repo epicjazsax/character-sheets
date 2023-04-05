654a3327e7d722f12547d8ab7d13ec063d5e9bf9
epicjazsax <justin.zakrajsek@gmail.com>
2023-04-05 09:16:48 -0700

    cleaned double and half proficiencies

--- roller.py
+++ roller.py
@@ -7,15 +7,17 @@ with open("brick.json", "r") as character_file:
 character = json.loads(character_string)
 attr = character["attributes"]
 mods = {
-    "str": (attr["str"]//2) - 5,
-    "dex": (attr["dex"]//2) - 5,
+    "str": (attr["str"] // 2) - 5,
+    "dex": (attr["dex"] // 2) - 5,
     "con": (attr["con"] // 2) - 5,
     "int": (attr["int"] // 2) - 5,
     "wis": (attr["wis"] // 2) - 5,
     "cha": (attr["cha"] // 2) - 5,
 }
 
-mods["prof"] = (character["level"]+7)//4
+mods["prof"] = (character["level"] + 7) // 4
+mods["dblprof"] = mods["prof"] * 2
+mods["halfprof"] = mods["prof"] // 2
 mods["init"] = mods["dex"]
 mods["acrobatics"] = mods["dex"]
 mods["animal_handling"] = mods["wis"]
@@ -36,10 +38,16 @@ mods["sleight"] = mods["dex"]
 mods["stealth"] = mods["dex"]
 mods["survival"] = mods["wis"]
 
-mods["dblprof"] = mods["prof"]*2
-mods["halfprof"] = mods["prof"]//2
-# todo
-# add check to skills-mod list to look at json file for proficiencies
+for item in character["proficiencies"]:
+    if "_dbl" in item:
+        item = item.strip("_dbl")
+        mods[item] = mods[item] + mods["dblprof"]
+    elif "_half" in item:
+        item = item.strip("_half")
+        mods[item] = mods[item] + mods["halfprof"]
+    else:
+        mods[item] = mods[item] + mods["prof"]
+
 
 print(f"Character attributes are {attr}")
-print(f"Attribute mods are {mods}")
+print(f"Attribute and skill mods are {mods}")
